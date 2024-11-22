1a1ac3dd7c6b4f35aea8a5ed663457e74f31af6a
shumail786-hash <sherrymines@outlook.com>
2024-11-22 21:52:34 +0500

    updated: aspect-ratio for mobile and tablet device in work section

--- src/pages/MyWork/MyWork.jsx
+++ src/pages/MyWork/MyWork.jsx
@@ -57,7 +57,7 @@ const MyWork = () => {
                   "0px 0px 0 #10e956, -1px -1px 0 #10e956, 1px -1px 0 #10e956, -1px 1px 0 #10e956, 1px 1px 0 #10e956",
               }}
             >
-              Polishing My Genius...
+              Fetching Projects 4 U!
             </p>
           ) : (
             projects.map((project, index) => (
@@ -74,7 +74,7 @@ const MyWork = () => {
                     <img
                       src={`${CLOUDINARY_URL}/${project.projectThumbnail}`}
                       alt={`${project.projectTitle}`}
-                      className="hover:scale-125 duration-300 bg-center h-full w-full aspect-[13/9]"
+                      className="hover:scale-125 duration-300 bg-center h-full w-full aspect-[10/10] lg:aspect-[13/9]"
                       loading="eager"
                     />
                   </div>
@@ -101,61 +101,6 @@ const MyWork = () => {
             ))
           )}
         </div>
-
-        {/* <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-x-2 gap-y-3 mt-10">
-          {loading ? (
-            <p
-              className="font-cyborg w-full col-span-3 text-center text-[3rem] tracking-widest"
-              style={{
-                textShadow:
-                  "0px 0px 0 #10e956, -1px -1px 0 #10e956, 1px -1px 0 #10e956, -1px 1px 0 #10e956, 1px 1px 0 #10e956",
-              }}
-            >
-              Polishing My Genius...
-            </p>
-          ) : (
-            projects &&
-            projects.map((project, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, scale: 0.9 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
-                viewport={{ once: false, amount: 0.2 }}
-              >
-                <div className="bg-neutral-900 py-3 px-3 rounded-md text-white">
-                  <div className="aspect-[13/9] w-full overflow-hidden rounded-md">
-                    <img
-                      src={`${CLOUDINARY_URL}/${project.projectThumbnail}`}
-                      alt={`${project.projectTitle}`}
-                      className="hover:scale-125 duration-300 bg-center h-full w-full"
-                      loading="eager"
-                    />
-                  </div>
-                  <div className="p-2">
-                    <p className="mt-3 font-exo tracking-[2px] text-3xl font-bold">
-                      {project.projectTitle}
-                    </p>
-                    <p className="py-2 font-exo tracking-[1.1px] text-gray-300 text-base">
-                      {project.projectDescription}
-                    </p>
-
-                    <div className="flex gap-x-2 gap-y-2 py-2 flex-wrap">
-                      {project.tags.map((elem, index) => (
-                        <span
-                          className="text-sm bg-backgroundColor text-bodyColor rounded-md px-3 py-1 font-sans"
-                          key={index}
-                        >
-                          {elem.name}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </motion.div>
-            ))
-          )}
-        </div> */}
       </ContentWrapper>
     </div>
   );
